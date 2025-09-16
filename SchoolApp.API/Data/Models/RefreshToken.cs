7317de0cc676aad8202d3abeb4754904e1ec7955
pohleliphiwe <pohleliphiwe@gmail.com>
2025-09-16 18:35:03 +0200

    Added the RefreshTokens table

---
+++ SchoolApp.API/Data/Models/RefreshToken.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SchoolApp.API.Data.Models
+{
+    public class RefreshToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public string JwtId { get; set; }
+        public bool IsRevoked { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateExpire { get; set; }
+
+        // Foreign Key
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]// not strictly necessary but good practice
+        public ApplicationUser User { get; set; }
+    }
+}
