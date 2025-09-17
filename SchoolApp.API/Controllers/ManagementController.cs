8b85c1a11c2ec104286cba2b0c72f7eab16fccd2
pohleliphiwe <pohleliphiwe@gmail.com>
2025-09-17 11:18:01 +0200

    Adding role authentication to a controller.

--- SchoolApp.API/Controllers/ManagementController.cs
+++ SchoolApp.API/Controllers/ManagementController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolApp.API.Data.Helpers;
 
 namespace SchoolApp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = UserRoles.Manager)]
     public class ManagementController : ControllerBase
     {
         public ManagementController()
--- SchoolApp.API/Controllers/StudentController.cs
+++ SchoolApp.API/Controllers/StudentController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SchoolApp.API.Data.Helpers;
 
 namespace SchoolApp.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(Roles = UserRoles.Student)]
     public class StudentController : ControllerBase
     {
         public StudentController()
